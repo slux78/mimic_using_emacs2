8c4a4e6022278ff82249389f33b50918cdf8e0d7
manime <slux78@gmail.com>
2017-03-05 05:20:57 +0900

    add ace-window setting

--- init.el
+++ init.el
@@ -5,13 +5,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org-bullets try use-package))))
+ '(package-selected-packages (quote (ace-window org-bullets try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
 ;; my code
 (setq inhibit-startup-message t)
@@ -54,3 +54,12 @@
 ;  :ensure t
 ;  :config
 ;  (tabbar-mode 1))
+
+(use-package ace-window
+  :ensure t
+  :init
+  (progn
+    (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-face-foreground :height 3.0)))))))
